79dc3456ce07cc13787feda8bb93511ad48a4c50
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-12 11:29:16 +0530

    basic component is created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './headers/header.component';
 
 @Component({
-  //how component should be identified or referenced
-  //selector helps in that
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+//
 export class AppComponent {
-  // this is data binding
-  //basically we are binding our data in html file
   title = 'practise-app';
 }
 
-// Bootstrapping is the process of initializing or loading angular application
+//to create component
+//1)Create a Typescript class and export it
+//2)Decorate the class with @Component decorator
+//3)Declare the class in main module file
+
+//IMPORTANT
+//child component folder should be always be inside parent component folder
+// IMPORTANT
+//Component is nothing but typescript class
