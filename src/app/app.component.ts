1ec4a8e0758fabb5cd4876a607b5f72efed77e4c
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-18 11:53:34 +0530

    basic example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,16 @@ import { ProductListComponent } from './product-list/product-list.component';
   styleUrl: './app.component.css',
 })
 
-// in product list component
-// used string interpolation
-//and also called function from template file
+//
+//Property binding lets us bind a property of a DOM object
+//for example the hidden property to some data value.
+//This can let us show or hide a DOM element, or manipulate
+//the DOM in some other way
+//when we want to display some data in HTML we use string interpolation
+//BUT
+// when we want to assign some dynamic value we to an HTML attribute
+//there we use property binding
+//in the "" we can write any typescript expression
 export class AppComponent {
   title = 'practise-app';
   message = 'online-learning';
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,12 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  name = 'John Doe';
   product = {
     name: 'iPhone',
     price: 999,
     color: 'Matte Black',
     discountedPrice: 8.5,
     inStock: 0,
+    pImg: '../../assets/images/messi.jpg',
   };
 
   getDiscountedPrice() {
