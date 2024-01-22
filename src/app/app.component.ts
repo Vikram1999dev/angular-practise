feff217f1f53fd5055ffbebb635d5842cfd8c789
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-22 10:45:42 +0530

    directive overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,25 @@ import { ProductListComponent } from './product-list/product-list.component';
 })
 
 //
-//data flow from view to class
-//use of event binding
+// a directive is an instructions to the DOM
+//it
+//manipulate DOM
+//change behavior
+//Add/Remove DOM elements
+
+//there are three types of directive that is
+
+//component directive
+//this directive is the angular component.it is a directive with a template
+//components are also a directive
+
+//attribute directive
+//this directive is used to change the appearance or behavior of a DOM element
+//e.g. ngClass and ngStyle
+
+//structural directive
+//this directive is used to add or remove a DOM element on the Webpage
+//e.g. ngIf,ngFor,ngSwitch
 export class AppComponent {
   title = 'practise-app';
   message = 'online-learning';
