2b60c3c6c76e08626c60320e64f5755b7a8b594f
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-27 18:08:15 +0530

    removed misleading comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,14 @@ import { TopHeaderComponent } from './top-header/top-header.component';
 })
 
 //to generate component using cli
-// we type command ng g c topHeader
-//
-//
-// to create child component of any component
-// you have to go inside the component folder
-// and type that cli command
-//
+// we type command " ng g c topHeader "
 //
 // for child component to be used inside the parent component
 //call the child component selector inside the parent html file
 //and also import its class in parent's ts file
 //
 // *********************************************
+//
 //I created a component folder outside its parent
 // earlier the child component should be created inside the parent component folder
 //to be used AND NOW that is not the case.
